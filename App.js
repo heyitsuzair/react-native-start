c90ec34164cb82e3d4d334a2b8c727c664970bad
heyitsuzair <uzairdevil354123@gmail.com>
2022-09-14 23:11:16 +0500

    Styles

--- App.js
+++ App.js
@@ -7,16 +7,29 @@
  */
 
 import React from 'react';
-import {Text, View} from 'react-native';
-import Login from './components/Login';
+import {Text, View, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
     <View>
-      <Text>Hello World!</Text>
-      <Login />
+      <Text style={[styles.red, styles.fonts]}>Just Red</Text>
+      <Text style={[styles.others, styles.fonts]}>Just Blue</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  red: {
+    color: 'red',
+    backgroundColor: 'green',
+  },
+  fonts: {
+    fontSize: 30,
+  },
+  others: {
+    color: 'blue',
+    backgroundColor: 'yellow',
+  },
+});
 
 export default App;
